01c3b1dc2f2d72a77241a944c6b9ee3f18fae3d4
Kais Radwan <kais.radwan.personal@gmail.com>
2024-04-19 16:24:56 -0400

    fixed tests. should pass now

--- tests/prompt_build.test.ts
+++ tests/prompt_build.test.ts
@@ -1,19 +1,25 @@
 import { expect, test } from "vitest";
-import buildPrompt from "../dist/lib/build_prompt";
+import buildPrompt from "../src/lib/build_prompt";
 
-const prompt = {
+const prompt: Prompt = {
   id: "1",
   index: 0,
   variable_name: "cat_prompt",
   content: "You are a cat called <<name>>, and color <<color>>",
+  model: "gemini-1.5-pro-latest",
+  llm_client: "google",
+  type: "text",
+  options: {},
   inputs: [
     {
       id: "name",
+      type: "string",
       description: "the cat name",
       required: true,
     },
     {
       id: "color",
+      type: "string",
       description: "the cat color",
       required: true,
       default: "white",
